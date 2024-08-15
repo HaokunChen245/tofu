67cc00816ceec9be37143fc75b4d95914443f541
Chen, Haokun <haokun.chen@intel.com>
2024-08-15 01:43:15 -0700

    add model

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, pipeli
 import os
 
 # MODEL_NAME = "meta-llama/Llama-2-7b-hf"
-# MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B"
+MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 # MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
-MODEL_NAME = "NousResearch/Llama-2-7b-chat-hf" #"microsoft/Phi-3-mini-4k-instruct"
+# MODEL_NAME = "NousResearch/Llama-2-7b-chat-hf" #"microsoft/Phi-3-mini-4k-instruct"
 # MODEL_NAME = "EleutherAI/gpt-j-6B"
 
 if os.environ.get('LOCAL_RANK') is not None:
@@ -14,7 +14,7 @@ if os.environ.get('LOCAL_RANK') is not None:
 else:
     device_map = None
 
-model_path = '/home/haokunch/data/locus/llm_weights/zhilif/TOFU/ft_epoch5_lr1e-05_llama2-7b_full_wd0.01/checkpoint-312'
+model_path = '/home/haokunch/data/locus/llm_weights/zhilif/TOFU/ft_epoch5_lr1e-05_llama3.1-8b-inst_full_wd0.01/checkpoint-312'
 config = AutoConfig.from_pretrained(MODEL_NAME, 
                                     trust_remote_code = True, 
                                     device_map=device_map)
@@ -28,12 +28,9 @@ model, tok = (
     ).to("cuda"),
     AutoTokenizer.from_pretrained(MODEL_NAME),
 )
-chat = [
-    {"role": "system", "content": "You are a helpful AI."},
-    {"role": "user", "content": "Who is Jaime Vasquez?"}
-]
 
-input_strings = ['[INST] When was Jaime Vasquez borned? [/INST]']
+input_strings = ['<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n  \
+                 You are a helpful assistant.<|eot_id|><|start_header_id|>user<|end_header_id|>When was Jaime Vasquez borned? <|eot_id|><|start_header_id|>assistant<|end_header_id|>']
 left_pad_tokenizer = tok
 left_pad_tokenizer.padding_side = 'left'
 left_pad_tokenizer.padding_size = 'longest'
@@ -42,7 +39,7 @@ left_pad_tokenizer.pad_token_id = left_pad_tokenizer.eos_token_id
 
 inputs = left_pad_tokenizer.batch_encode_plus(input_strings, add_special_tokens=True, return_tensors='pt', padding=True).to(model.device)
 #now generate
-out = model.generate(inputs.input_ids, attention_mask=inputs.attention_mask, max_length=100, max_new_tokens=100, do_sample=False, use_cache=True, pad_token_id=left_pad_tokenizer.eos_token_id)
+out = model.generate(inputs.input_ids, attention_mask=inputs.attention_mask, max_length=100, do_sample=False, use_cache=True, pad_token_id=left_pad_tokenizer.eos_token_id)
 strs = left_pad_tokenizer.batch_decode(out[:, inputs.input_ids.shape[-1]:], skip_special_tokens=True)
 
 print(strs)
